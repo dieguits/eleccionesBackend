cedf97bc6bd055836e4ebeebde58d4013576ee29
Diego Perez <dapohp@gmail.com>
2021-04-27 18:30:23 -0400

    some changes to solve the heroku deployment

--- dist/src/routes/representante.routes.js
+++ dist/src/routes/representante.routes.js
@@ -1,7 +1,11 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = require("express");
 var representante_controller_1 = require("../controllers/representante.controller");
+var cors_1 = __importDefault(require("cors"));
 var router = express_1.Router();
-router.route("/").get(representante_controller_1.getRepresents).post(representante_controller_1.createRepresent);
+router.route("/").get(representante_controller_1.getRepresents, cors_1.default()).post(representante_controller_1.createRepresent, cors_1.default());
 exports.default = router;
--- src/routes/representante.routes.ts
+++ src/routes/representante.routes.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import { getRepresents, createRepresent } from "../controllers/representante.controller";
+import cors from 'cors';
 
 const router = Router();
 
-router.route("/").get(getRepresents).post(createRepresent);
+router.route("/").get(getRepresents, cors()).post(createRepresent, cors());
 
 export default router;
